1dfcbcaf258aa4e77a630501e40a839cb0c07325
TimTen10 <Tim.Eckle94@gmail.com>
2021-10-16 17:36:25 +0200

    Manga init layout changed.

--- util.py
+++ util.py
@@ -1,11 +1,20 @@
+from datetime import datetime
+
+
 class Manga:
 
-    def __init__(self, name, latest_chapter, latest_update, latest_check, url):
+    def __init__(self,
+                 name: str,
+                 url: str,
+                 latest_chapter: int = None,
+                 latest_update: datetime = None,
+                 latest_check: datetime = None):
         self.name = name
         self.latest_chapter = latest_chapter
         self.latest_update = latest_update
         self.latest_check = latest_check
         self.url = url
+        self.need_update = True  # On creation an update is needed, then after each X amount of time.
 
     def __str__(self):
         return f"Name: {self.name} ({self.url}), latest chapter: {self.latest_chapter} on {self.latest_update}"
