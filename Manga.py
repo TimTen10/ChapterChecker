b42b9554cb9e5437c9b9e23b61401dabf69128d7
TimTen10 <Tim.Eckle94@gmail.com>
2021-10-19 19:03:39 +0200

    Basic classes and structure.

--- Manga.py
+++ Manga.py
@@ -6,15 +6,17 @@ class Manga:
     def __init__(self,
                  name: str,
                  url: str,
+                 provider: str = None,
                  latest_chapter: int = None,
                  latest_update: datetime = None,
                  latest_check: datetime = None):
         self.name = name
+        self.url = url
+        self.provider = provider
         self.latest_chapter = latest_chapter
         self.latest_update = latest_update
         self.latest_check = latest_check
-        self.url = url
-        self.need_update = True  # On creation an update is needed, then after each X amount of time.
+        self.need_check = True  # On creation a check for update is needed, then after every X amount of time interval.
 
     def __str__(self):
         return f"Name: {self.name} ({self.url}), latest chapter: {self.latest_chapter} on {self.latest_update}"
---
+++ MangaList.py
@@ -0,0 +1,13 @@
+from Manga import Manga
+
+
+class MangaList:
+
+    def __init__(self, source):
+        # creates a new manga list from a source file
+        # TODO: we need two constructor methods -> from file and empty list
+        self.source = source
+
+    def add_manga(self, url):
+        # Takes in an URL and adds the corresponding manga to the manga list object.
+        pass
