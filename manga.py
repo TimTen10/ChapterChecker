97cbcfc313522baff1e8eb76ae0a0f18873257e3
TimTen10 <Tim.Eckle94@gmail.com>
2021-10-20 18:30:29 +0200

    Added method ideas.

--- manga.py
+++ manga.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import List
 
 
 class Manga:
@@ -6,17 +7,24 @@ class Manga:
     def __init__(self,
                  name: str,
                  url: str,
+                 rating: int = None,
+                 genres: List[int] = None,
                  provider: str = None,
                  latest_chapter: int = None,
                  latest_update: datetime = None,
                  latest_check: datetime = None):
         self.name = name
         self.url = url
+        self.rating = rating
+        self.genres = genres
         self.provider = provider
         self.latest_chapter = latest_chapter
         self.latest_update = latest_update
         self.latest_check = latest_check
         self.need_check = True  # On creation a check for update is needed, then after every X amount of time interval.
+
+    def set_rating(self, value):
+        self.rating = value
 
     def __str__(self):
         return f"Name: {self.name} ({self.url}), latest chapter: {self.latest_chapter} on {self.latest_update}"
--- manga_list.py
+++ manga_list.py
@@ -8,6 +8,13 @@ class MangaList:
         # TODO: we need two constructor methods -> from file and empty list
         self.source = source
 
-    def add_manga(self, url):
+    def add_manga_single(self, url):
         # Takes in an URL and adds the corresponding manga to the manga list object.
+        pass
+
+    def add_manga_list(self, url_list):
+        pass
+
+    def sort_by(self):
+        # Manga list should be sortable by latest release, rating, genres, etc.
         pass
