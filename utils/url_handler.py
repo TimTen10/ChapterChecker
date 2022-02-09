c9a7e030d6119d7064ee1c9ac671b033c0a331ba
TimTen10 <Tim.Eckle94@gmail.com>
2022-02-09 21:20:39 +0100

    Clean up methods finished.

--- utils/url_handler.py
+++ utils/url_handler.py
@@ -1,35 +1,39 @@
+import fileinput
+
+
 def clean_up_mangakakalot(manga_url):
     if 'chapter' in manga_url:
         provider, _, ending = manga_url.partition('/chapter/')
-        return provider + '/manga/' + ending.split('/')[0] + '\n'
+        return provider + '/manga/' + ending.split('/')[0]
     else:
-        return manga_url
+        return manga_url.strip()
 
 
 def clean_up_manganelo(manga_url):
     if 'chapter' in manga_url:
-        return manga_url
+        provider, _, ending = manga_url.partition('/chapter/')
+        return provider + '/manga/' + ending.split('/')[0]
     else:
-        return manga_url
+        return manga_url.strip()
 
 
 def clean_up_manganato(manga_url):
     if 'chapter' in manga_url:
-        return manga_url
+        return manga_url.split('/chapter-')[0]
     else:
-        return manga_url
+        return manga_url.strip()
 
 
 def clean_up_manga_list_file(manga_list_file):
     # clean up each line in the mangalists file
-    with open(f'./mangalists/{manga_list_file}.txt', 'r+') as ml_file:
-        for manga_url in ml_file.readlines():
+    with fileinput.input(f'./mangalists/{manga_list_file}.txt', inplace=True) as manga_f:
+        for manga_url in manga_f:
             if 'mangakakalot' in manga_url:
-                manga_url = clean_up_mangakakalot(manga_url)
+                print(clean_up_mangakakalot(manga_url))
             elif 'manganelo' in manga_url:
-                manga_url = clean_up_manganelo(manga_url)
+                print(clean_up_manganelo(manga_url))
             elif 'readmanganato' in manga_url:
-                manga_url = clean_up_manganato(manga_url)
+                print(clean_up_manganato(manga_url))
             else:
                 raise ValueError(f'Provider not supported {manga_url}.')
 
