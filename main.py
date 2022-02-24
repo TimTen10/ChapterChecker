22b43d6b78302e257b60ed3acb72bbf8a91b5786
TimTen10 <Tim.Eckle94@gmail.com>
2022-02-24 16:37:15 +0100

    Functionality for checking a single manga (url)

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import argparse
 
 from manga_list import MangaList, load_manga_list
 
+from utils.manga_parser import parse_manga_update
 from utils.url_handler import clean_up_manga_list_file
 
 
-def main(**kwargs):
+
+def handle_parser(**kwargs):
     # TODO: do not allow multiple arguments at once
     # create > add > check
     if kwargs['create_empty']:
@@ -29,7 +31,9 @@ def main(**kwargs):
 
     if kwargs['check_single']:
         # Checks the manga (by url) for updates in the last "time" hours
-        pass
+        chapter, url, latest_update, _ = parse_manga_update(kwargs['check_single'])
+        print(f'Chapter {chapter} got released! -> {url}'
+              f'Date: {latest_update.strftime("%H:%M %d.%m.%Y ")}')
 
     if kwargs['check_list']:
         # Updates / Checks a whole manga list
@@ -62,8 +66,7 @@ if __name__ == "__main__":
     parser.add_argument('--add_manga_list', nargs=2, help='manga list name followed by manga urls filename')
 
     # argument for checking single manga for most recent update
-    parser.add_argument('--check_single', nargs=2,
-                        help='url of manga that you want checked followed by time window to check')
+    parser.add_argument('--check_single', help='url of manga that you want checked followed by time window to check')
 
     # argument for checking a manga list for most recent updates
     parser.add_argument('--check_list', help='name of the manga list you want checked')
@@ -73,4 +76,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(**vars(args))
+    handle_parser(**vars(args))
